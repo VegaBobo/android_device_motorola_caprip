a4ac08d46fcc8659b5234c9fd7b31d95741a47e4
VegaBobo <vegazstestando@gmail.com>
2022-03-22 18:42:48 -0300

    Add installer

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,6 +102,8 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
+USE_RECOVERY_INSTALLER := true
+RECOVERY_INSTALLER_PATH := device/motorola/caprip/installer
 
 # Modules
 TW_LOAD_VENDOR_MODULES := "exfat.ko nova_0flash_mmi.ko"
