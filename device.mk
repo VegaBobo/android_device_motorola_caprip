5ff88cc51fed8c9e0f4fc9e2aba6c04a66d4562c
VegaBobo <vegazstestando@gmail.com>
2022-03-22 16:45:24 -0300

    Make it at least buildable

--- device.mk
+++ device.mk
@@ -38,12 +38,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     bootctrl.bengal
 
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.bengal \
-    libgptutils \
-    libz \
-    libcutils
-
 PRODUCT_PACKAGES += \
     otapreopt_script \
     cppreopts.sh \
